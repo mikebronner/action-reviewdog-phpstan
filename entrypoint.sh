9ac84a515d52c62d5615b362ea8463c3d72484d0
Mike Bronner <mike@genealabs.com>
2020-10-11 22:35:58 +0000

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,5 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-php /usr/local/bin/phstan.phar analyse ${INPUT_TARGET_DIRECTORY} --level=max --memory-limit 1G --error-format=raw \
+php /usr/local/bin/phpstan.phar analyse ${INPUT_TARGET_DIRECTORY} --level=max --memory-limit 1G --error-format=raw \
     | reviewdog -name=PHPStan -f=phpstan -reporter=${INPUT_REPORTER} -level=${INPUT_LEVEL} -diff='git diff'
